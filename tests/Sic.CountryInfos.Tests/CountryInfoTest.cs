be4514d0347ed668661e2c13e6c1fb6245aba49a
Simon Bordewisch <SiC42@users.noreply.github.com>
2025-03-22 16:54:42 +0100

    Rework ID generation
    * Rework ID generation
    
    * Fix tests

--- tests/Sic.CountryInfos.Tests/CountryInfoTest.cs
+++ tests/Sic.CountryInfos.Tests/CountryInfoTest.cs
@@ -1,10 +1,45 @@
+using Sic.CountryInfos.Enums;
+
 namespace Sic.CountryInfos.Tests;
 
 public class CountryInfoTest
 {
     [Fact]
-    public Task Test()
+    public void Country_AllValuesCorrectlyInCountryInfo()
     {
-        return Verify(LanguageInfo.All);
+       Assert.Equal(Enum.GetValues<Country>().Length, CountryInfo.All.Count);
+       Assert.Multiple(() =>
+       {
+           foreach (var country in Enum.GetValues<Country>())
+           {
+               Assert.Equal(country, CountryInfo.Get(country).Country);
+           }
+       });
+    }
+
+    [Fact]
+    public void CountryIso2Code_AllValuesCorrectlyInCountryInfo()
+    {
+       Assert.Equal(Enum.GetValues<CountryIso2Code>().Length, CountryInfo.All.Count);
+       Assert.Multiple(() =>
+       {
+           foreach (var country in Enum.GetValues<CountryIso2Code>())
+           {
+               Assert.Equal(country, CountryInfo.Get(country).TwoLetterIsoCode);
+           }
+       });
+    }
+
+    [Fact]
+    public void CountryIso3Code_AllValuesCorrectlyInCountryInfo()
+    {
+       Assert.Equal(Enum.GetValues<CountryIso3Code>().Length, CountryInfo.All.Count);
+       Assert.Multiple(() =>
+       {
+           foreach (var country in Enum.GetValues<CountryIso3Code>())
+           {
+               Assert.Equal(country, CountryInfo.Get(country).ThreeLetterIsoCode);
+           }
+       });
     }
 }
--- tests/Sic.CountryInfos.Tests/LanguageInfoTest.cs
+++ tests/Sic.CountryInfos.Tests/LanguageInfoTest.cs
@@ -1,22 +1,45 @@
+using Sic.CountryInfos.Enums;
+
 namespace Sic.CountryInfos.Tests;
 
 public class LanguageInfoTest
 {
     [Fact]
-    public void Value_OfLanguageIso2AndIso3Code_AreIdentical()
+    public void LocaleCode_AllValuesCorrectlyInCountryInfo()
     {
+        Assert.Equal(Enum.GetValues<LocaleCode>().Length, LanguageInfo.All.SelectMany(l => l.LocaleCodes).Count());
         Assert.Multiple(() =>
         {
-            foreach (var languageInfo in LanguageInfo.All)
+            foreach (var language in Enum.GetValues<LocaleCode>())
             {
-                Assert.Equal((int)languageInfo.LanguageIso2Code, (int)languageInfo.LanguageIso3Code);
+                Assert.Contains(language, LanguageInfo.Get(language).LocaleCodes);
             }
         });
     }
 
     [Fact]
-    public Task Test()
+    public void LanguageIso2Code_AllValuesCorrectlyInCountryInfo()
     {
-        return Verify(LanguageInfo.All);
+        Assert.Equal(Enum.GetValues<LanguageIso2Code>().Length, LanguageInfo.All.Count);
+        Assert.Multiple(() =>
+        {
+            foreach (var language in Enum.GetValues<LanguageIso2Code>())
+            {
+                Assert.Equal(language, LanguageInfo.Get(language).LanguageIso2Code);
+            }
+        });
+    }
+
+    [Fact]
+    public void LanguageIs3Code_AllValuesCorrectlyInCountryInfo()
+    {
+        Assert.Equal(Enum.GetValues<LanguageIso3Code>().Length, LanguageInfo.All.Count);
+        Assert.Multiple(() =>
+        {
+            foreach (var language in Enum.GetValues<LanguageIso3Code>())
+            {
+                Assert.Equal(language, LanguageInfo.Get(language).LanguageIso3Code);
+            }
+        });
     }
 }
